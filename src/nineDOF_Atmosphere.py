6dabe324080b027cc611dd59e731f009a3bd573b
petergsherman <petersherman37@gmail.com>
2025-12-03 11:48:28 -0600

    Going to transfer atmosphere to this file and add dynamic effects

--- src/nineDOF_Atmosphere.py
+++ src/nineDOF_Atmosphere.py
@@ -1,2 +1,16 @@
 #nineDOF_Atmosphere.py
 
+from dataclasses import dataclass
+import numpy as np
+import random 
+
+@dataclass
+class staticAtmosphere:
+    DEN: float = 1.22566     # Air density (kg/m^3)
+    VXWIND: float = 0.0      # Wind velocity X (m/s)
+    VYWIND: float = 0.0      # Wind velocity Y (m/s)
+    VZWIND: float = 0.0      # Wind velocity Z (m/s)
+
+class dynamicAtmosphere:
+    def __init__(self) -> None:
+        pass
